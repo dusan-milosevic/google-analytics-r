77fd6c3d05366a6c225600e3829b2dc1733e35e7
Dusan Milosevic <32218168+dusan-milosevic@users.noreply.github.com>
2018-12-30 18:20:26 +0100

    Create 1_setup.r

---
+++ 1_setup.r
@@ -0,0 +1,25 @@
+## Install libraries
+install.packages("googleAuthR")
+install.packages("googleAnalyticsR")
+install.packages("tidyverse")
+
+## Load libraries
+library("googleAuthR")
+library("googleAnalyticsR")
+library("tidyverse")
+
+## Authorization with GA GA servers
+ga_auth()
+
+## Google Analytics Account List
+account_list <- google_analytics_account_list()
+
+## Google Analytics View ID 
+GA_id <- XXXXXXXX
+
+## GA Query
+ga <- google_analytics(GA_id, 
+                       date_range = c("2018-01-01", "2018-12-31"),  
+                       metrics = c("sessions"), 
+                       dimensions = c("date"),
+                       anti_sample = TRUE)
